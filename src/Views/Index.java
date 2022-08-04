f7dfacef30d1e55ac8e8cfb3ab7c495a03d938e5
willman orellano <wilmanjunior2001@gmail.com>
2022-08-04 01:58:42 -0500

    se configuro el proyecto y se creo la ventana principal

---
+++ src/Views/Index.java
@@ -0,0 +1,9 @@
+package Views;
+
+public class Index {
+    public static void main(String[] args) {
+        
+        Layout layout = new Layout();
+        layout.setVisible(true);
+    }   
+}
--- src/Views/Layout.java
+++ src/Views/Layout.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Views;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
  * @author wjorellano
  */
-public class Layout {
-    
+public class Layout extends JFrame{
+
+    public Layout(){
+        setSize(500,500);
+        setTitle("Aplicacion");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        panelComponent();
+    }
+
+    public void panelComponent(){
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.WHITE);
+        // add(panel);
+        this.getContentPane().add(panel);
+    }
 }
